8df46e0fc28e048c64593a10fa292fa443843684
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-03-29 18:53:13 -0300

    implemented the first action and mutation for change the initial state products of store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,29 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
-  state: {
-    
-  },
-  mutations: {
-    
-  },
-  actions: {
 
+  state: {
+    products: []
   },
+
+  mutations: {
+    loadProducts(state, products) {
+      console.log(products)
+      state.products = products
+    }
+  },
+
+  actions: {
+    loadProducts({ commit }) {
+      axios.get('https://fakestoreapi.com/products')
+      .then(resp => {
+        commit('loadProducts', resp.data)
+      })
+    }
+  },
+
   modules: {
   }
+
 })
